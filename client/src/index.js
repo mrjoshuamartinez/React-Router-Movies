c8c48a31229e797fc30957fd32ad4f4ae6892667
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-09-05 10:47:32 -0400

    Wrapping Router around App

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Browserrouter, { Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    // Wrapping ARouter around App
+    <Router>
+        <App />
+    </Router>
+
+, 
+document.getElementById('root'));
